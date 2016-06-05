4edd889dda82998a99c72e217560e9be08b8940f
tsukimizake <shomasd@gmail.com>
2016-06-06 08:19:48 +0900

    change signature of downloader

--- src/Downloader.hs
+++ src/Downloader.hs
@@ -2,7 +2,7 @@ module Downloader (download) where
 import Network.HTTP.Simple
 import Network.HTTP.Conduit
 import Control.Applicative
-import Data.ByteString.Lazy
+import Data.ByteString.Lazy.Char8
 
-download :: String -> IO ByteString
-download s = return . getResponseBody =<< httpLBS =<< parseUrl s
+download :: String -> IO String
+download s = return . unpack . getResponseBody =<< httpLBS =<< parseUrl s
